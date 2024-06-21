b164d6e2f4d53f40e1a3f106f8e8c6feea787fec
kimyoda <rladygks1210@naver.com>
2024-06-22 03:09:53 +0900

    [feat] setion02_5 Gugudan webpack bulid, setting

--- 1.구구단/client.jsx
+++ 1.구구단/client.jsx
@@ -1,2 +1,7 @@
+// client.jsx
 const React = require("react");
-const ReactDom = require("react-dom");
+const ReactDOM = require("react-dom");
+
+const GuGuDan = require("./GuGuDan");
+
+ReactDOM.render(<GuGuDan />, document.querySelector("#root"));
