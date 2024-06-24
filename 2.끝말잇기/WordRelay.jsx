2d6725d683c6a6a7a88578b9fb83bbad3bdd685a
kimyoda <rladygks1210@naver.com>
2024-06-25 02:43:02 +0900

    [feat] setion02_9 Controlled Input, Uncontrolled Input

--- 2.끝말잇기/WordRelay.jsx
+++ 2.끝말잇기/WordRelay.jsx
@@ -27,6 +27,40 @@ const WordRelay = () => {
     }
   };
 
+  // 컨트롤드 인풋(Controoled Input)
+  /**
+   * React 상태(state)와 연결된 인풋, 컴포넌트의 상태에 의해 제어
+   * const ControlledInput = () => {
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={value} onChange={handleChange} />
+   */
+
+  // 언컨트롤드 인풋(Uncontrolled Input)
+  /*
+  * React 상태와 독립적으로 DOM에 직접 값을 관리하는 인풋이다. 기본값을 설정할 땐
+  'defaultValue'를 사용하고, 'value'를 사용하면 컨트롤드 인풋으로 간주한다.
+const UncontrolledInput = () => {
+  const inputRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Current Value: ${inputRef.current.value}`);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={inputRef} />
+            </div>
+   */
+
   const onChangeInput = (e) => {
     setValue(e.target.value);
   };
@@ -39,7 +73,7 @@ const WordRelay = () => {
           id="wordInput"
           ref={inputRef}
           value={value}
-          onChange={onChangeInput}
+          onChange={(e) => setValue(e.current.value)}
         />
         <button>입력!</button>
       </form>
