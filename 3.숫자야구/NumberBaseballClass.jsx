37caf53f53f55282871dbd3c77ee44a5cda6820a
kimyoda <rladygks1210@naver.com>
2024-06-26 02:25:15 +0900

    [feat] setion03_4 Componet, Props

--- 3.숫자야구/NumberBaseballClass.jsx
+++ 3.숫자야구/NumberBaseballClass.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Try from "./Try";
 
 function getNumber() {
   // 숫자 4개를 겹치지 않고 랜덤하게 뽑는 함수
@@ -16,6 +17,14 @@ class NumberBaseball extends Component {
 
   onChangeInput = () => {};
 
+  fruits = [
+    { fruit: "사과", taste: "맛있다" },
+    { fruit: "감", taste: "시다" },
+    { fruit: "귤", taste: "달다" },
+    { fruit: "밤", taste: "떫다" },
+    { fruit: "배", taste: "맛없다" },
+  ];
+
   render() {
     return (
       <>
@@ -29,18 +38,8 @@ class NumberBaseball extends Component {
         </form>
         <div>시도: {this.state.tries.length}</div>
         <ul>
-          {[
-            { fruit: "사과", taste: "맛있다" },
-            { fruit: "감", taste: "시다" },
-            { fruit: "귤", taste: "달다" },
-            { fruit: "밤", taste: "떫다" },
-            { fruit: "배", taste: "맛없다" },
-          ].map((v, i) => {
-            return (
-              <li key={v.fruit + v.taste}>
-                <b>{v.fruit}</b> - {i}
-              </li>
-            );
+          {this.fruits.map((v, i) => {
+            return <Try value={v} index={i} />;
           })}
         </ul>
       </>
