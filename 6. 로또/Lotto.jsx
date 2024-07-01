af3c92c0afd419a5b3197ce17888c7b14de9f7c8
kimyoda <rladygks1210@naver.com>
2024-07-02 02:33:30 +0900

    [feat] setion06_03 Lotto React Hooks transfer, useEffect update

---
+++ 6. 로또/Lotto.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useRef, useState } from "react";
+import Ball from "./Ball";
+
+function getWinNumbers() {
+  console.log("getWinNumbers");
+  const candidate = Array(45)
+    .fill()
+    .map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(
+      candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
+    );
+  }
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+  return [...winNumbers, bonusNumber];
+}
+
+// runtimeout 변환
+useEffect(() => {
+  for (let i = 0; i < winNumbers.length - 1; i++) {
+    timeouts.current[i] = setTimeout(() => {
+      setWinBalls((prevBalss) => [...prevBalss, winNumbers[i]]);
+    }, (i + 1) * 1000);
+  }
+  timeouts.current[6] = setTimeout(() => {
+    setBonus(winNumbers[6]);
+    setRedo(true);
+  }, 7000);
+  return () => {
+    timeouts.current.forEach((v) => {
+      clearTimeout(v);
+    });
+  };
+}, [timeouts.current]); //input 자리가 빈배열이면 componetDidMount와 같다.
+// 배열에 요소가 있으면 componetDidMount, componenetDidUpdate 둘 다 수행한다.
+
+const Lotto = () => {
+  const [winNumbers, setWinNumbers] = useState(getWinNumbers());
+  const [winBalls, setWinBalls] = useState([]);
+  const [bonus, setBonus] = useState(null);
+  const [redo, setRedo] = useState(false);
+  const timeouts = useRef([]);
+
+  const onClickRedo = () => {
+    console.log("onClickRedo");
+    setWinNumbers(getWinNumbers());
+    setWinBalls([]);
+    setBonus(null);
+    setRedo(false);
+    timeouts.current = [];
+  };
+
+  return (
+    <>
+      <div>당첨 숫자</div>
+      <div id="결과창">
+        {winBalls.map((v) => (
+          <Ball key={v} number={v} />
+        ))}
+      </div>
+      <div>보너스!</div>
+      {bonus && <Ball number={bonus} />}
+      {redo && <button onClick={onClickRedo}>한 번 더!</button>}
+    </>
+  );
+};
+
+export default Lotto;
--- 6. 로또/client.jsx
+++ 6. 로또/client.jsx
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import Lotto from "./Lotto";
-import LottoClass from "./LottoClass";
 
-ReactDOM.createRoot(document.querySelector("#root")).render(<LottoClass />);
+ReactDOM.createRoot(document.querySelector("#root")).render(<Lotto />);
