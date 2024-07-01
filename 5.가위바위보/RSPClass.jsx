aaf9a67125b373b563ef33bd7813d71464fe7778
kimyoda <rladygks1210@naver.com>
2024-07-01 14:18:33 +0900

    [feat] setion05_02 RSP React setInterval

--- 5.가위바위보/RSPClass.jsx
+++ 5.가위바위보/RSPClass.jsx
@@ -4,16 +4,46 @@ import React, { Component } from "react";
 // -> (setState/props 바뀔 때 -> render -> componenetDidUpdate)
 //  부모가 나를 없앴을 때 -> componentWillUnmount -> 소멸
 
-class RSP extends Component {
+const rspCoords = {
+  바위: "0",
+  가위: "-142px",
+  보: "-284px",
+};
+
+const scores = {
+  가위: 1,
+  바위: 0,
+  보: -1,
+};
+
+class RSPClass extends Component {
   state = {
     result: "",
-    imgCoord: 0,
+    imgCoord: rspCoords.바위,
     score: 0,
   };
 
+  interval;
+
   // 클래스 라이프사이클
   componentDidMount() {
-    // 컴포넌트가 첫 렌더링된 후
+    // 컴포넌트가 첫 렌더링된 후 -> 비동기 요청을 많이 한다.
+    this.interval = setInterval(() => {
+      const { imgCoord } = this.state;
+      if (imgCoord === rspCoords.바위) {
+        this.setState({
+          imgCoord: rspCoords.가위,
+        });
+      } else if (imgCoord === rspCoords.가위) {
+        this.setState({
+          imgCoord: rspCoords.보,
+        });
+      } else if (imgCoord === rspCoords.보) {
+        this.setState({
+          imgCoord: rspCoords.바위,
+        });
+      }
+    }, 1000);
   }
 
   componentDidUpdate() {
@@ -21,8 +51,11 @@ class RSP extends Component {
   }
 
   componentWillUnmount() {
-    // 컴포넌트가 제거되기 직전
+    // 컴포넌트가 제거되기 직전, 비동기 요청 정리를 많이 한다.
+    clearInterval(this.interval);
   }
+
+  onClickBtn = () => {};
 
   render() {
     const { result, score, imgCoord } = this.state;
@@ -56,4 +89,4 @@ class RSP extends Component {
   }
 }
 
-export default RSP;
+export default RSPClass;
