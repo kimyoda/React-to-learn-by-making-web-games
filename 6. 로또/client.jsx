0e7af9f00ce1c3a7b8b0085ddc48358f8465962c
kimyoda <rladygks1210@naver.com>
2024-07-01 23:19:18 +0900

    [feat] setion06_01 Lotto React Component

---
+++ 6. 로또/client.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import Lotto from "./Lotto";
+import LottoClass from "./LottoClass";
+
+ReactDOM.createRoot(document.querySelector("#root")).render(<LottoClass />);
