cc58d2f8c608e48f77ae1daba9575f86e74e9aef
kimyoda <rladygks1210@naver.com>
2024-07-02 22:15:11 +0900

    [feat] setion07_02 TicTacToe action crate, dispatch

--- 7. 틱택토/TicTacToe.jsx
+++ 7. 틱택토/TicTacToe.jsx
@@ -11,7 +11,9 @@ const initalState = {
   ],
 };
 
-const SET_WINNER = "SET_WINNER";
+export const SET_WINNER = "SET_WINNER";
+export const CLICK_CELL = "CLICK_CELL";
+export const CHANGE_TURN = "CHANGE_TURN";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -21,6 +23,21 @@ const reducer = (state, action) => {
         ...state,
         winner: action.winner,
       };
+    case CLICK_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...tableData[action.row]]; // immer라는 라이브러리로 가독성 문제 해결
+      tableData[action.row][action.cell] = state.turn;
+      return {
+        ...state,
+        tableData,
+      };
+    }
+    case CHANGE_TURN: {
+      return {
+        ...state,
+        turn: state.turn === "O" ? "X" : "O",
+      };
+    }
   }
 };
 
@@ -41,7 +58,11 @@ const TicTacTeo = () => {
 
   return (
     <>
-      <Table onClick={onClickTable} tableData={state.tableData} />
+      <Table
+        onClick={onClickTable}
+        tableData={state.tableData}
+        dispatch={dispatch}
+      />
       {state.winner && <div>{state.winner}님의 승리</div>}
     </>
   );
