aa66a119bfd7861ed012028739f2468803f447f5
kimyoda <rladygks1210@naver.com>
2024-07-03 23:35:49 +0900

    [feat] setion08_02 MineSearch React useContext, Provider

--- 8. 지뢰찾기/Form.jsx
+++ 8. 지뢰찾기/Form.jsx
@@ -1,9 +1,11 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useContext, useState } from "react";
+import { TableContext } from "./MineSearch";
 
 const Form = () => {
   const [row, setRow] = useState(10);
   const [cell, setCell] = useState(10);
   const [mine, setMine] = useState(10);
+  const { dispatch } = useContext(TableContext);
 
   const onChangeRow = useCallback((e) => {
     setRow(e.target.value);
@@ -17,7 +19,9 @@ const Form = () => {
     setMine(e.target.value);
   }, []);
 
-  const onClickBtn = useCallback(() => {}, []);
+  const onClickBtn = useCallback(() => {
+    dispatch({ type: START_GAME, row, cell, mine });
+  }, [row, cell, mine]);
 
   return (
     <div>
--- 8. 지뢰찾기/MineSearch.jsx
+++ 8. 지뢰찾기/MineSearch.jsx
@@ -1,6 +1,28 @@
-import React, { useReducer } from "react";
+import React, { createContext, useMemo, useReducer } from "react";
 import Table from "./Table";
 import Form from "./Form";
+
+export const CODE = {
+  MINE: -7,
+  NORMAL: -1,
+  FLAG: -3,
+  QUESTION: -2,
+  QUESTION_MINE: -4,
+  FLAG_MINE: -5,
+  CLICKED_MINE: -6,
+  OPENED: 0, // 0 이상이면 전부 OPENED로 변경
+};
+
+const TabelContext = createContext({
+  tabelData: [
+    [-1, -1, -1, -1, -1, -1, -1],
+    [-7, -1, -1, -1, -1, -1, -1],
+    [-1, -7, -1, -7, -7, -1, -1],
+    [],
+    [],
+  ],
+  dispatch: () => {},
+});
 
 const initialState = {
   tabelData: [],
@@ -8,22 +30,40 @@ const initialState = {
   result: "",
 };
 
+export const START_GAME = "START_GAME";
+
 const reducer = (state, action) => {
   switch (action.type) {
+    case START_GAME:
+      return {
+        ...state,
+        tabelData: plantMine(action.row, action.cell, action.mine),
+      };
     default:
       return state;
   }
 };
 
 const MineSearch = () => {
+  // ContextApi 캐싱사용
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const value = useMemo(
+    () => ({
+      tabelData: state.tabelData,
+      dispatch,
+    }),
+    [state.tabelData]
+  );
 
   return (
     <>
-      <Form />
-      <div>{state.timer}</div>
-      <Table />
-      <div>{state.result}</div>
+      <TabelContext.Provider value={value}>
+        <Form />
+        <div>{state.timer}</div>
+        <Table />
+        <div>{state.result}</div>
+      </TabelContext.Provider>
     </>
   );
 };
