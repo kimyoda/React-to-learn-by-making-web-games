ceb0aa5199541197d8ecc4f0f148085a7a8a3330
kimyoda <rladygks1210@naver.com>
2024-07-04 15:02:09 +0900

    [feat] setion08_04 MineSearch React OnRightClickBtn, Mine Check to number add

--- 8. 지뢰찾기/MineSearch.jsx
+++ 8. 지뢰찾기/MineSearch.jsx
@@ -15,6 +15,7 @@ export const CODE = {
 
 export const TableContext = createContext({
   tableData: [],
+  halted: true,
   dispatch: () => {},
 });
 
@@ -22,6 +23,7 @@ const initialState = {
   tableData: [],
   timer: 0,
   result: "",
+  halted: true,
 };
 
 const plantMine = (row, cell, mine) => {
@@ -58,6 +60,11 @@ const plantMine = (row, cell, mine) => {
 };
 
 export const START_GAME = "START_GAME";
+export const OPEN_CELL = "OPEN_CELL";
+export const CLICK_MINE = "CLICK_MINE";
+export const FLAG_CELL = "FLAG_CELL";
+export const QUESTION_CELL = "QUESTION_CELL";
+export const NORMALIZE_CELL = "NORMALIZE_CELL";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -65,7 +72,87 @@ const reducer = (state, action) => {
       return {
         ...state,
         tableData: plantMine(action.row, action.cell, action.mine),
+        halted: false,
       };
+    case OPEN_CELL:
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      let around = [];
+      if (tableData[action.row - 1]) {
+        around = around.concat(
+          tableData[action.row - 1][action.cell - 1],
+          tableData[action.row - 1][action.cell],
+          tableData[action.row - 1][action.cell + 1]
+        );
+      }
+      around = around.concat(
+        tableData[action.row][action.cell - 1],
+        tableData[action.row][action.cell + 1]
+      );
+      if (tableData[action.row + 1]) {
+        around = around.concat(
+          tableData[action.row + 1][action.cell - 1],
+          tableData[action.row + 1][action.cell],
+          tableData[action.row + 1][action.cell + 1]
+        );
+      }
+      const count = around.filter((v) =>
+        [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v)
+      ).length;
+      tableData[action.row][action.cell] = count;
+      return {
+        ...state,
+        tableData,
+      };
+    case CLICK_MINE: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      tableData[action.row][action.cell] = CODE.CLICKED_MINE;
+      return {
+        ...state,
+        tableData,
+        halted: true,
+      };
+    }
+    case FLAG_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if (tableData[action.row][action.cell] === CODE.MINE) {
+        tableData[action.row][action.cell] = CODE.FLAG_MINE;
+      } else {
+        tableData[action.row][action.cell] = CODE.FLAG;
+      }
+      return {
+        ...state,
+        tableData,
+      };
+    }
+    case QUESTION_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if (tableData[action.row][action.cell] === CODE.FLAG_MINE) {
+        tableData[action.row][action.cell] = CODE.QUESTION_MINE;
+      } else {
+        tableData[action.row][action.cell] = CODE.QUESTION;
+      }
+      return {
+        ...state,
+        tableData,
+      };
+    }
+    case NORMALIZE_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      if (tableData[action.row][action.cell] === CODE.QUESTION_MINE) {
+        tableData[action.row][action.cell] = CODE.MINE;
+      } else {
+        tableData[action.row][action.cell] = CODE.NORMAL;
+      }
+      return {
+        ...state,
+        tableData,
+      };
+    }
     default:
       return state;
   }
@@ -74,22 +161,24 @@ const reducer = (state, action) => {
 const MineSearch = () => {
   // ContextApi 캐싱사용
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { tableData, halted, timer, result } = state;
 
   const value = useMemo(
     () => ({
-      tableData: state.tableData,
+      tableData: tableData,
       dispatch,
+      halted: halted,
     }),
-    [state.tableData]
+    [tableData, halted]
   );
 
   return (
     <>
       <TableContext.Provider value={value}>
         <Form />
-        <div>{state.timer}</div>
+        <div>{timer}</div>
         <Table />
-        <div>{state.result}</div>
+        <div>{result}</div>
       </TableContext.Provider>
     </>
   );
